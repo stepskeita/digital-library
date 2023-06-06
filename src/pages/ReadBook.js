10e413ecaa2cd10176facbc666d48f3ca7d058e0
elsalvacion <elsalvacion2022@gmail.com>
2023-06-06 21:44:53 +0600

    pdf read structured

--- src/pages/ReadBook.js
+++ src/pages/ReadBook.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import Container from "../components/layout/Container";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { BOOKS } from "../constants/books";
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
-const ReadBook = ({}) => {
+const ReadBook = () => {
   const { slug } = useParams();
   const history = useHistory();
 
@@ -24,7 +23,13 @@ const ReadBook = ({}) => {
 
   return (
     <>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <button
+        onClick={() => history.goBack()}
+        className="block w-fit mb-2 ml-5 p-2 rounded-lg bg-sky-500 hover:bg-sky-600 text-white"
+      >
+        GO BACK
+      </button>
+      <Worker workerUrl="/js/pdf.worker.min.js">
         <div style={{ height: "750px" }}>
           <Viewer fileUrl={book.book} />
         </div>
